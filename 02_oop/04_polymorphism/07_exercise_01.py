a8ca43462e5e6bdf673995e16704d4f91bed2018
Doan Thanh Nhan <nhandt@hybrid-technologies.vn>
2021-04-07 14:13:53 +0700

    Add polymorphism exercises

---
+++ 02_oop/04_polymorphism/07_exercise_01.py
@@ -0,0 +1,20 @@
+# Parent Class
+class Shape:
+    sname = "Shape"
+
+    def getName(self):
+        return self.sname
+
+
+# child class
+class XShape(Shape):
+    # initializer
+    def __init__(self, name):
+        self.xsname = name
+
+    def getName(self):  # overriden method
+        return (super().getName() + ", " + self.xsname)
+
+
+circle = XShape("Circle")
+print(circle.getName())
