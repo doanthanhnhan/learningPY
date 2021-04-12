eaab5a90c6b0705860e64b099e346121420b6e70
Doan Thanh Nhan <doanthanhnhan@outlook.com>
2021-04-12 16:14:55 +0700

    Add multiple inheritance exercise

---
+++ 02_oop/03_inheritance/10_multiple_inheritance_01.py
@@ -0,0 +1,52 @@
+class Dog():
+  def sound(self):
+    print("woof woof")
+
+  def soundD(self):
+    print("dog dog")
+
+
+class Cat():
+  def sound(self):
+    print("meow meow")
+
+  def soundC(self):
+    print("cat cat")
+
+
+class Duck():
+  def sound(self):
+    print("quack quack")
+
+  def soundDuck(self):
+    print("duck duck")
+
+
+class DogCat(Dog, Cat):
+  def __init__(self):
+    pass
+
+  def soundDC(self):
+    super().soundD()
+    super().soundC()
+
+
+class DogCatDuck(DogCat, Duck):
+  def soundDCD(self):
+    super().soundD()
+    super().soundC()
+    super().soundDuck()
+
+
+dc = DogCat()
+print("=== Sound ===")
+dc.sound()
+print("=== SoundDC ===")
+dc.soundDC()
+# test.display1()
+
+dcd = DogCatDuck()
+print("=== Sound ===")
+dcd.sound()
+print("=== SoundDCD ===")
+dcd.soundDCD()
